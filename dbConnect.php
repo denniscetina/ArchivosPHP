6da905ceaf89a4a1d4ccbb1e81d9519bbbd71f6d
Sofia Abigail Carrillo Ek <sofia.abigail.carrillo@gmail.com>
2020-04-22 21:42:54 -0500

    Delete dbConnect.php

--- dbConnect.php
+++
@@ -1,9 +0,0 @@
-<?php 
- define('HOST','db793449398.hosting-data.io');
- define('USER','dbo793449398');
- define('PASS','Roalom_72');
- define('DB','db793449398');
- 
- $con = mysqli_connect(HOST,USER,PASS,DB) 
- or die('unable to connect to db');
- ?>
