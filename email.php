d4b9e3a03f53c44a4a4b7495f3a18d0cd1f79246
Sofia Abigail Carrillo Ek <sofia.abigail.carrillo@gmail.com>
2020-04-22 21:43:44 -0500

    Update email.php

--- email.php
+++ email.php
@@ -1,7 +1,7 @@
 <?php
 
 //NO TOCAR ES IMPORTANTE
-$DBhost = "ps-mysqldb.cqarvpef8c0c.us-east-1.rds.amazonaws.com";
+$DBhost = "ps-prod-mysql.cqarvpef8c0c.us-east-1.rds.amazonaws.com";
 $DBuser = "discereUsr";
 $DBpass = "!!Discere123";
 $DBname = "discere_db-dev";
